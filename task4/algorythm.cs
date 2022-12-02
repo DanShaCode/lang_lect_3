58b1edfead26afe982c5af19fff8984aa590d4c5
Danis Sharipov <danshadance@mail.ru>
2022-12-02 11:59:21 +0600

    Конец лекции

---
+++ task4/algorythm.cs
@@ -0,0 +1,38 @@
+﻿// Сортировка массива 
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7};
+
+void PrintArray (int[] arr)
+{
+    int count = arr.Length;
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"{arr[i]}");
+}
+Console.WriteLine();
+}
+
+void SelectionSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length-1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[j] < arr[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = arr[i];
+        arr[i] = arr[minPosition];
+        arr[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+
