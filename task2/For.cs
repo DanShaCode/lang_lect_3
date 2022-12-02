50207538ab57b28be7dba3651e1a408214f3d383
Danis Sharipov <danshadance@mail.ru>
2022-12-01 20:11:19 -0800

    19:06 лекция

--- task2/For.cs
+++ task2/For.cs
@@ -9,15 +9,28 @@
 
 // Пример =======================================================================
 
-string Method4(int count, string c)
-{
-    string result = String.Empty;
-    for (int i = 0; i < count; i++)
-    {
-        result = result + c;
-    }
-    return result;
-}
+// string Method4(int count, string c)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + c;
+//     }
+//     return result;
+// }
 
-string res = Method4(10, "asdasd");
-Console.WriteLine(res);
+// string res = Method4(10, "asdasd");
+// Console.WriteLine(res);
+
+// Цикл в Цикле =========================================================================================
+
+// Таблица умножения =========== ПРИМЕР ======================
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <=10 ; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
